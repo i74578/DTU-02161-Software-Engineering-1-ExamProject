f117feb480348c281d430157c5767bd4925b9e50
lukashalberg <121937418+lukashalberg1@users.noreply.github.com>
2023-05-03 15:42:03 +0200

    register time

--- src/test/java/cucumber/TimeSteps.java
+++ src/test/java/cucumber/TimeSteps.java
@@ -1,13 +1,55 @@
 //author: Christian Colberg - s224343
+//author: Lukas Halberg - s216229
 package cucumber;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import timeCat.application.TimeCatApp;
 
 import static org.junit.Assert.assertNull;
 
 public class TimeSteps {
+
+    private TimeCatApp timeCatApp;
+    private ErrorMessage errorMessage;
+    private TimeSteps timeSteps;
+
+
+
+    public TimeSteps(TimeCatApp timeCatApp, ErrorMessage errorMessage) {
+        this.timeCatApp = timeCatApp;
+        this.errorMessage = errorMessage;
+
+    }
+
+    @When("the employee registers hours spent on activity")
+    public void theEmployeeRegistersHoursSpentOnActivity() {
+    }
+
+
+    @Then("the hours are registered")
+    public void theHoursAreRegistered() {
+    }
+
+    @And("an employee is assigned activity")
+    public void anEmployeeIsAssignedActivity() {
+    }
+
+    @And("an employee is not assigned activity")
+    public void anEmployeeIsNotAssignedActivity() {
+    }
+
+
+
+
+
+
+
+
+
+
 
     @Given("employee has registered hours on activity")
     public void employeeHasRegisteredHoursOnActivity() {
@@ -31,4 +73,7 @@ public class TimeSteps {
 
 
     }
+
+
+
 }
