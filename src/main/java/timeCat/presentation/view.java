15e539ef00e197996b991eeaccc82a5976682045
benja <26153040+i74578@users.noreply.github.com>
2023-05-03 12:42:33 +0200

    Tiny refactoring

--- src/main/java/timeCat/presentation/view.java
+++ src/main/java/timeCat/presentation/view.java
@@ -12,6 +12,20 @@ public class view {
     private final static int cliWidth = 100;
 
     //@author  Benjamin Fríðberg - s224347
+    public void showWelcomeScreen(){
+        //Welcome screen ASCII art source: https://patorjk.com/software/taag/
+        String welcomeScreen =
+            " __      __          ___                                             __               __                            ____              __      \n" +
+            "/\\ \\  __/\\ \\        /\\_ \\                                           /\\ \\__           /\\ \\__  __                    /\\  _`\\           /\\ \\__   \n" +
+            "\\ \\ \\/\\ \\ \\ \\     __\\//\\ \\     ___    ___     ___ ___      __       \\ \\ ,_\\   ___    \\ \\ ,_\\/\\_\\    ___ ___      __\\ \\ \\/\\_\\     __  \\ \\ ,_\\  \n" +
+            " \\ \\ \\ \\ \\ \\ \\  /'__`\\\\ \\ \\   /'___\\ / __`\\ /' __` __`\\  /'__`\\      \\ \\ \\/  / __`\\   \\ \\ \\/\\/\\ \\ /' __` __`\\  /'__`\\ \\ \\/_/_  /'__`\\ \\ \\ \\/  \n" +
+            "  \\ \\ \\_/ \\_\\ \\/\\  __/ \\_\\ \\_/\\ \\__//\\ \\L\\ \\/\\ \\/\\ \\/\\ \\/\\  __/       \\ \\ \\_/\\ \\L\\ \\   \\ \\ \\_\\ \\ \\/\\ \\/\\ \\/\\ \\/\\  __/\\ \\ \\L\\ \\/\\ \\L\\.\\_\\ \\ \\_ \n" +
+            "   \\ `\\___x___/\\ \\____\\/\\____\\ \\____\\ \\____/\\ \\_\\ \\_\\ \\_\\ \\____\\       \\ \\__\\ \\____/    \\ \\__\\\\ \\_\\ \\_\\ \\_\\ \\_\\ \\____\\\\ \\____/\\ \\__/.\\_\\\\ \\__\\\n" +
+            "    '\\/__//__/  \\/____/\\/____/\\/____/\\/___/  \\/_/\\/_/\\/_/\\/____/        \\/__/\\/___/      \\/__/ \\/_/\\/_/\\/_/\\/_/\\/____/ \\/___/  \\/__/\\/_/ \\/__/\n\n";
+        System.out.println(welcomeScreen);
+    }
+
+    //@author  Benjamin Fríðberg - s224347
     public void clearConsole(){
         print("\n".repeat(20));
     }
@@ -32,24 +46,11 @@ public class view {
         System.out.print("\n Choose option:");
     }
 
-    //@author  Benjamin Fríðberg - s224347
-    public void showWelcomeScreen(){
-        //Welcome screen ASCII art source: https://patorjk.com/software/taag/
-        String welcomeScreen =
-        " __      __          ___                                             __               __                            ____              __      \n" +
-        "/\\ \\  __/\\ \\        /\\_ \\                                           /\\ \\__           /\\ \\__  __                    /\\  _`\\           /\\ \\__   \n" +
-        "\\ \\ \\/\\ \\ \\ \\     __\\//\\ \\     ___    ___     ___ ___      __       \\ \\ ,_\\   ___    \\ \\ ,_\\/\\_\\    ___ ___      __\\ \\ \\/\\_\\     __  \\ \\ ,_\\  \n" +
-        " \\ \\ \\ \\ \\ \\ \\  /'__`\\\\ \\ \\   /'___\\ / __`\\ /' __` __`\\  /'__`\\      \\ \\ \\/  / __`\\   \\ \\ \\/\\/\\ \\ /' __` __`\\  /'__`\\ \\ \\/_/_  /'__`\\ \\ \\ \\/  \n" +
-        "  \\ \\ \\_/ \\_\\ \\/\\  __/ \\_\\ \\_/\\ \\__//\\ \\L\\ \\/\\ \\/\\ \\/\\ \\/\\  __/       \\ \\ \\_/\\ \\L\\ \\   \\ \\ \\_\\ \\ \\/\\ \\/\\ \\/\\ \\/\\  __/\\ \\ \\L\\ \\/\\ \\L\\.\\_\\ \\ \\_ \n" +
-        "   \\ `\\___x___/\\ \\____\\/\\____\\ \\____\\ \\____/\\ \\_\\ \\_\\ \\_\\ \\____\\       \\ \\__\\ \\____/    \\ \\__\\\\ \\_\\ \\_\\ \\_\\ \\_\\ \\____\\\\ \\____/\\ \\__/.\\_\\\\ \\__\\\n" +
-        "    '\\/__//__/  \\/____/\\/____/\\/____/\\/___/  \\/_/\\/_/\\/_/\\/____/        \\/__/\\/___/      \\/__/ \\/_/\\/_/\\/_/\\/_/\\/____/ \\/___/  \\/__/\\/_/ \\/__/\n\n";
-        System.out.println(welcomeScreen);
-    }
 
     //@author  Benjamin Fríðberg - s224347
     public void printTableWithHeader(ArrayList<? extends Tabelify> tableElements,boolean printIndex,String tableHeader){
         print(HorizontalLine);
-        print(centerStringInTableRecord(tableHeader));
+        print(centerString(tableHeader));
         printTable(tableElements,printIndex);
     }
 
@@ -57,7 +58,7 @@ public class view {
     public void printTable(ArrayList<? extends Tabelify> tableContent, boolean printIndex){
         print(HorizontalLine);
         if(tableContent.size() == 0){
-            print(centerStringInTableRecord("Empty"));
+            print(centerString("Empty"));
         }
         for (int i = 0;i<tableContent.size();i++){
             String[] recordProperties = tableContent.get(i).getPropertiesForTable();
@@ -73,7 +74,7 @@ public class view {
     }
 
     //@author  Benjamin Fríðberg - s224347
-    public String centerStringInTableRecord(String stringToCenter){
+    public String centerString(String stringToCenter){
         int spacesNeedToFillWidth = cliWidth-stringToCenter.length() - 2;
         String leftSpacePadding = " ".repeat(spacesNeedToFillWidth / 2);
         String rightSpacePadding = " ".repeat((spacesNeedToFillWidth / 2)+(stringToCenter.length() % 2));
